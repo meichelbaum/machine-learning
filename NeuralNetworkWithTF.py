ee5898edec8470eeff02d1984f2b3163444b3410
nolanst71439 <stephannolan87@yahoo.degit config --global user.name Stephan>
2020-09-06 14:52:53 +0200

    commit from pycharm

--- NeuralNetworkWithTF.py
+++ NeuralNetworkWithTF.py
@@ -2,9 +2,8 @@ import numpy as np
 import pandas as pd
 import tensorflow as tf
 
-
 ann = tf.keras.models.Sequential()
 ann.add(tf.keras.layers.Dense(units=786, activation='sidmoid'))
 ann.add(tf.keras.layers.Dense(units=100, activation='sigmoid'))
 ann.add(tf.keras.layers.Dense(units=10, activation='sigmoid'))
-ann.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+ann.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
